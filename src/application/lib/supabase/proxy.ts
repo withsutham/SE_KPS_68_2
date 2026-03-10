8850563e7cb8a80e746bd271ad11f070770572ff
krittiyawadeeP <160371953+krittiyawadeeP@users.noreply.github.com>
2026-03-10 19:02:30 +0700

    remove unuse path name

--- src/application/lib/supabase/proxy.ts
+++ src/application/lib/supabase/proxy.ts
@@ -53,8 +53,7 @@ export async function updateSession(request: NextRequest) {
     !request.nextUrl.pathname.startsWith("/login") &&
     !request.nextUrl.pathname.startsWith("/auth") &&
     !request.nextUrl.pathname.startsWith("/api") &&
-    !request.nextUrl.pathname.startsWith("/booking") &&
-    !request.nextUrl.pathname.startsWith("/manager")
+    !request.nextUrl.pathname.startsWith("/booking")
   ) {
     // no user, potentially respond by redirecting the user to the login page
     const url = request.nextUrl.clone();
