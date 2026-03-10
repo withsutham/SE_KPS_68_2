68a73c960aa85cf07da6e1531007be4eefdf4e69
psrisuphan <phuditsaphat.sri@gmail.com>
2026-03-10 23:13:32 +0700

    fix(#72): remove empty space bottom padding and minimum layout heights on printed pages to prevent an extra blank page

--- src/application/app/layout.tsx
+++ src/application/app/layout.tsx
@@ -49,7 +49,7 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          <div id="app-root" className="min-h-screen flex flex-col bg-background text-foreground font-sans selection:bg-primary/30">
+          <div id="app-root" className="min-h-screen print:min-h-0 flex flex-col bg-background text-foreground font-sans selection:bg-primary/30">
             <Nav />
             {children}
             <Footer />
